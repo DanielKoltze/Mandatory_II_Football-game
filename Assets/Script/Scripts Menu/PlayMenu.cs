f4bf86087561f023577713e816e474f329269eee
DanielKoltze <danielkoltze@yahoo.dk>
2023-05-08 08:42:13 +0200

    Now you can hit esc to go to menu

--- Assets/Script/Scripts Menu/PlayMenu.cs
+++ Assets/Script/Scripts Menu/PlayMenu.cs
@@ -6,26 +6,9 @@ using TMPro;
 
 public class PlayMenu : MonoBehaviour
 {
-    private bool levelOneCleared = false;
-    private bool levelTwoCleared = false;
-    public GameObject levelTwoField;
-    public GameObject levelThreeField;
     public void LoadLevel(string levelName)
     {
-        /*
-        if(levelName == "Level1" ||
-            levelName == "Level2" && levelOneCleared ||
-            levelName == "Level3" && levelTwoCleared)
-        {
             SceneManager.LoadScene(levelName);
-        }
-        */
-        SceneManager.LoadScene(levelName);
     }
-    private void Start()
-    {
-        //levelTwoField.SetActive(levelOneCleared);
-        //levelThreeField.SetActive(levelTwoCleared);
-    }
-
+   
 }
